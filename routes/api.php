a88b1bd24ecc4227f341746383657f1fa49f474a
Luciano Veras <luciano.veras@hotmail.com>
2019-12-22 19:12:04 -0300

    [ADD] Rota para a batalha

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('personagem', 'PersonagemController');
-Route::resource('arma', 'ArmaController');
+Route::resource('batalha', 'BatalhaController');
