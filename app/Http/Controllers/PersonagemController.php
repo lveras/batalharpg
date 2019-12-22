88551d4edcaa3bdc24bea0ef74f1e16e5e720065
Luciano Veras <luciano.veras@hotmail.com>
2019-12-22 12:42:55 -0300

    [ADD] Controllers sem create e update

---
+++ app/Http/Controllers/PersonagemController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Personagem;
+use Illuminate\Http\Request;
+
+class PersonagemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Personagem  $personagem
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Personagem $personagem)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Personagem  $personagem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Personagem $personagem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Personagem  $personagem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Personagem $personagem)
+    {
+        //
+    }
+}
