4bc3338bc64dc04d6ed82cb8a95aa0215d52ac8c
Luciano Veras <luciano.veras@hotmail.com>
2019-12-23 17:45:59 -0300

    [REF] Regras de negócio no Controller

--- app/Rodada.php
+++ app/Rodada.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rodada extends Model
 {
+    protected $fillable = ['batalha_id', 'num_rodada', 'acao',
+        'valor_dado_p1', 'valor_dado_p2', 'dano'];
+    public $timestamps = false;
+
     public function batalha()
     {
         return $this->belongsTo(Batalha::class);
